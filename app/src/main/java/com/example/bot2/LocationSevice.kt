4f47c92d03bd94fc3c90e15d85e3223672495488
Mohamed Rshad <126785911+Mohamed11Rshad@users.noreply.github.com>
2023-11-08 19:33:37 +0200

    setup navigation button with gps

---
+++ app/src/main/java/com/example/bot2/LocationSevice.kt
@@ -0,0 +1,78 @@
+package com.example.bot2.services
+
+import android.Manifest
+import android.app.Activity
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+
+class LocationService(private val activity: Activity) {
+    private val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
+
+    fun checkLocationSettingsAndStartLocationUpdates(callback: (Location?) -> Unit) {
+        val locationSettingsRequest = buildLocationSettingsRequest(buildLocationRequest())
+        val settingsClient = LocationServices.getSettingsClient(activity)
+
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+            .addOnSuccessListener {
+                // All location settings are satisfied, start location updates
+                getCurrentLocation(callback)
+            }
+            .addOnFailureListener { exception ->
+                if (exception is ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed by showing the user a dialog
+                    try {
+                        exception.startResolutionForResult(activity, REQUEST_CHECK_SETTINGS)
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        // Ignore the error
+                    }
+                }
+            }
+    }
+
+    private fun buildLocationRequest(): LocationRequest = LocationRequest.create().apply {
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        interval = 0
+        fastestInterval = 0
+        numUpdates = 1
+    }
+
+    private fun buildLocationSettingsRequest(locationRequest: LocationRequest): LocationSettingsRequest {
+        return LocationSettingsRequest.Builder().addLocationRequest(locationRequest).build()
+    }
+
+    private fun getCurrentLocation(callback: (Location?) -> Unit) {
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+            return
+        }
+
+        // Directly request for the current location
+        requestNewLocationData(callback)
+    }
+
+    private fun requestNewLocationData(callback: (Location?) -> Unit) {
+        val locationRequest = buildLocationRequest()
+
+        try {
+            fusedLocationClient.requestLocationUpdates(locationRequest, object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    callback(locationResult.lastLocation)
+                    fusedLocationClient.removeLocationUpdates(this)
+                }
+            }, activity.mainLooper)
+        } catch (unlikely: SecurityException) {
+            Log.e("LocationService", "Lost location permission. Could not request updates. $unlikely")
+        }
+    }
+
+    companion object {
+        const val REQUEST_LOCATION_PERMISSION = 1
+        const val REQUEST_CHECK_SETTINGS = 2
+    }
+}
--- app/src/main/java/com/example/bot2/MainActivity.kt
+++ app/src/main/java/com/example/bot2/MainActivity.kt
@@ -7,32 +7,51 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bot2.adapters.DrawerAdapter
 import com.example.bot2.databinding.ActivityMainBinding
-
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.example.bot2.services.LocationService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var locationService: LocationService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        windowSetup()
+        locationService = LocationService(this)
+
         drawerSetup()
+        windowSetup()
 
 
+        binding.powerBtnCard.setOnClickListener {
+            requestLocationAndUpdateUI()
+        }
     }
 
-    fun drawerSetup(){
+    fun drawerSetup() {
 
 
         val drawerItems = listOf(
-            DrawerItem("Tubby Do" , R.drawable.ic_tubby_do),
+            DrawerItem("Tubby Do", R.drawable.ic_tubby_do),
             DrawerItem("Empty Tubby", R.drawable.ic_empty_tubby),
             DrawerItem("Add Device", R.drawable.ic_add_device),
             DrawerItem("Preferences", R.drawable.ic_prefrences),
@@ -46,8 +65,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         //when an drawer item is clicked
-        drawerAdapter.onClick = {itemPosition ->
-            Toast.makeText(this, "Clicked on ${drawerItems[itemPosition].title}", Toast.LENGTH_SHORT).show()
+        drawerAdapter.onClick = { itemPosition ->
+            Toast.makeText(
+                this,
+                "Clicked on ${drawerItems[itemPosition].title}",
+                Toast.LENGTH_SHORT
+            ).show()
         }
 
 
@@ -70,13 +93,78 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun windowSetup(){
+    fun windowSetup() {
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
     }
+    private fun requestLocationAndUpdateUI() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationService.checkLocationSettingsAndStartLocationUpdates { location ->
+                updateLocationUI(location)
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LocationService.REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
 
+    private fun updateLocationUI(location: Location?) {
+        location?.let {
+            Log.d("LocationLog", "Lat: ${it.latitude}\nLng: ${it.longitude}")
+            Toast.makeText(this, "Lat: ${it.latitude}\nLng: ${it.longitude}", Toast.LENGTH_SHORT).show()
+        } ?: Log.d("LocationLog", "Location is null")
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LocationService.REQUEST_LOCATION_PERMISSION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                requestLocationAndUpdateUI()
+            } else {
+                showRationaleDialog()
+            }
+        }
+    }
+
+    private fun showRationaleDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("This app needs location permission to function. Please enable it in settings.")
+            .setPositiveButton("Settings") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
+    }
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == LocationService.REQUEST_CHECK_SETTINGS) {
+            if (resultCode == RESULT_OK) {
+                locationService.checkLocationSettingsAndStartLocationUpdates { location ->
+                    location?.let {
+                        Log.d("LocationLog", "Lat: ${it.latitude}\nLng: ${it.longitude}")
+                    } ?: Log.d("LocationLog", "Location is null")
+                    Toast.makeText(this,"lat: ${location?.latitude}\nlng: ${location?.longitude}",Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                // User chose not to make required location settings changes.
+                Log.d("LocationLog", "User chose not to make required location settings changes.")
+            }
+        }
+    }
 
 }
+
 
